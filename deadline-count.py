17e05788da2838040c360e31cb467e99f3912bec
x091204 <akifmuhammed009@gmail.com>
2026-03-22 22:53:55 +0530

    updated with notes

---
+++ deadline-count.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+user_input = input("enter your goal with the dead line spearated by colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = (datetime.datetime.strptime(deadline,"%d.%m.%Y"))
+todays_date = (datetime.datetime.today())
+# calculation of the deadline from today to how many days from today
+time_remain = deadline_date - todays_date
+print(f"dear user your time remaining for the deadline of {goal} is {time_remain.days} days")
